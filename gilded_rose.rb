52fab451c4aafda97ed27000c1326a2b7afc5905
Michael Ries <michael@riesd.com>
2014-05-15 14:32:15 -0600

    simplifying conditionals
    * use a case statement for the outer branching
    * change nested ifs to use inline conditionals

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,40 +1,29 @@
 def update_quality(items)
   items.each do |item|
-    if item.name == "Aged Brie"
-      item.sell_in -= 1
+    case item.name
+    when "Sulfuras, Hand of Ragnaros"
+    when "Aged Brie"
       item.quality += 1
-      if item.sell_in <= 0
-        item.quality += 1
-      end
+      item.quality += 1 if item.sell_in <= 0
       item.quality = 50 if item.quality > 50
-    elsif item.name == "Sulfuras, Hand of Ragnaros"
-
-    elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
-      if item.sell_in <= 0
-        item.quality = 0
-      elsif item.sell_in <= 5
-        item.quality += 3
-      elsif item.sell_in <= 10
-        item.quality += 2
-      else
-        item.quality += 1
-      end
       item.sell_in -= 1
-      item.quality = 50 if item.quality > 50
-    elsif item.name == "Conjured Mana Cake"
-      if item.sell_in <= 0
-        item.quality -= 2
-      end
+    when "Backstage passes to a TAFKAL80ETC concert"
+      item.quality += 1
+      item.quality += 1  if item.sell_in <= 10
+      item.quality += 1  if item.sell_in <= 5
+      item.quality  = 50 if item.quality > 50
+      item.quality  = 0  if item.sell_in <= 0
+      item.sell_in -= 1
+    when "Conjured Mana Cake"
       item.quality -= 2
+      item.quality -= 2 if item.sell_in <= 0
       item.sell_in -= 1
-      item.quality = 0 if item.quality < 0
-    else #Normal Case
+      item.quality  = 0 if item.quality < 0
+    else
       item.sell_in -= 1
       item.quality -= 1
-      if item.sell_in <= 0
-        item.quality -= 1
-      end
-      item.quality = 0 if item.quality < 0
+      item.quality -= 1 if item.sell_in <= 0
+      item.quality  = 0 if item.quality < 0
     end
   end
 end
